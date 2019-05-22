e9a0fdea41e38a9fd05e837f2a8275097b8c78a7
Deam Diaz <jdamian27@gmail.com>
2019-05-22 18:54:41 -0400

    Add edit and remove functions. Enable and use animations

--- src/app/schedule-list/schedule-list.component.ts
+++ src/app/schedule-list/schedule-list.component.ts
@@ -1,53 +1,142 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {ModalDirective} from "angular-bootstrap-md";
-import {FormControl} from "@angular/forms";
+import {Component, ViewChild} from '@angular/core';
+import {ModalDirective} from 'angular-bootstrap-md';
+import {FormControl} from '@angular/forms';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
+export class Schedule {
+  constructor(public id: number, public time: string, public subject: string, public location?: string,
+              public description?: string, public state: string = 'inactive') {
+  }
 
-export interface Schedule {
-  id?: string;
-  time: string;
-  subject: string;
-  location?: string;
-  description?: string;
+  toggleState() {
+    this.state = this.state === 'active' ? 'inactive' : 'active';
+  }
 }
 
 @Component({
   selector: 'app-schedule-list',
   templateUrl: './schedule-list.component.html',
-  styleUrls: ['./schedule-list.component.css']
+  styleUrls: ['./schedule-list.component.css'],
+  animations: [
+    trigger('scheduleState', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.02)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class ScheduleListComponent {
   @ViewChild(ModalDirective) modal: ModalDirective;
 
-  timeInput = new FormControl();
-  subjectInput = new FormControl();
-  locationInput = new FormControl();
-  descriptionInput = new FormControl();
+  selectedSchedule: Schedule;
+  items: Array<Schedule>;
+  index: number;
+  timeInput: FormControl;
+  subjectInput: FormControl;
+  locationInput: FormControl;
+  descriptionInput: FormControl;
 
-  items: Array<Schedule> = [
-    {time: '08:00', subject: 'Breakfast with Simon', location: 'Lounge Caffe', description: 'Discuss Q3 targets'},
-    {time: '08:30', subject: 'Daily Standup Meeting (recurring)', location: 'Warsaw Spire Office'},
-    {time: '09:00', subject: 'Call with HRs'},
-    {
-      time: '12:00', subject: 'Lunch with Timmoty', location: 'Canteen',
-      description: 'Project evalutation ile declaring a variable and using an if statement is a fine way to conditionally render a component, sometimes you might want to use a'
-    }
-  ];
+  idInputEdit: FormControl;
+  timeInputEdit: FormControl;
+  subjectInputEdit: FormControl;
+  locationInputEdit: FormControl;
+  descriptionInputEdit: FormControl;
+
+  constructor() {
+    this.index = 0;
+    this.timeInput = new FormControl();
+    this.subjectInput = new FormControl();
+    this.locationInput = new FormControl();
+    this.descriptionInput = new FormControl();
+
+    this.idInputEdit = new FormControl();
+    this.timeInputEdit = new FormControl();
+    this.subjectInputEdit = new FormControl();
+    this.locationInputEdit = new FormControl();
+    this.descriptionInputEdit = new FormControl();
+
+    this.items = [];
+  }
 
   addNewItem() {
-    const value: Schedule = {
-      time: this.timeInput.value,
-      subject: this.subjectInput.value,
-      location: this.locationInput.value,
-      description: this.descriptionInput.value
-    };
-    this.items.push(value);
-    this.timeInput.setValue('');
-    this.subjectInput.setValue('');
-    this.locationInput.setValue('');
-    this.descriptionInput.setValue('');
+    const value: Schedule = new Schedule(
+      this.index++,
+      this.timeInput.value,
+      this.subjectInput.value,
+      this.locationInput.value,
+      this.descriptionInput.value
+    );
+    if (!this.items.includes(value)) {
+      this.items.push(value);
+    }
+    this.resetFormAdd();
     this.modal.hide();
   }
 
+  editSchedule(item: Schedule, frame: any) {
+    const elem = this.items.find(sch => sch.id === item.id);
+    if (elem) {
+      this.idInputEdit.setValue(elem.id);
+      this.timeInputEdit.setValue(elem.time);
+      this.subjectInputEdit.setValue(elem.subject);
+      this.locationInputEdit.setValue(elem.location);
+      this.descriptionInputEdit.setValue(elem.description);
+      frame.show();
+    }
+  }
 
+  saveEditSchedule(frame: any) {
+    const elem = this.items.find(sch => sch.id === this.idInputEdit.value);
+    if (elem) {
+      elem.time = this.timeInputEdit.value;
+      elem.subject = this.subjectInputEdit.value;
+      elem.location = this.locationInputEdit.value;
+      elem.description = this.descriptionInputEdit.value;
+      frame.hide();
+    }
+    this.resetFormEdit();
+  }
+
+  deleteSchedule(item: Schedule) {
+    if (this.items.includes(item)) {
+      this.items = this.items.filter(todo => todo.id !== item.id);
+      return this;
+    }
+  }
+
+  onSelect(schedule: Schedule) {
+    this.selectedSchedule = schedule;
+  }
+
+  resetFormAdd() {
+    this.timeInput.reset();
+    this.subjectInput.reset();
+    this.locationInput.reset();
+    this.descriptionInput.reset();
+  }
+
+  resetFormEdit() {
+    this.idInputEdit.reset();
+    this.timeInputEdit.reset();
+    this.subjectInputEdit.reset();
+    this.locationInputEdit.reset();
+    this.descriptionInputEdit.reset();
+  }
+
+  toggleState(item) {
+    for (const elem of this.items) {
+      if (elem.id === item.id) {
+        item.toggleState();
+      } else {
+        elem.state = 'inactive';
+      }
+    }
+  }
 }
